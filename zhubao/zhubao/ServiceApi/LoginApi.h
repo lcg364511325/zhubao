f43d92a9a264b2be254fb48bd1ea8f9431e2330c
moko <caiguo@saikokutekiMac.local>
2014-06-06 03:15:27 -0700

    222

--- zhubao/zhubao/ServiceApi/LoginApi.h
+++ zhubao/zhubao/ServiceApi/LoginApi.h
@@ -10,17 +10,12 @@
 #import "LoginEntity.h"
 #import "AppDelegate.h"
 #import "DataService.h"
+#import "getNowTime.h"
+#import "Commons.h"
 
 @interface LoginApi : NSObject
 
 -(LoginEntity*)login:(NSString *)username password:(NSString *)password verlity:(NSString*)verlity;
 
-//-(NSMutableArray*)GetNews_yejhd:(int)Page typeid:(int)typeid;
-//
-//-(id)GetNews_content:(NSString *)newid;
-//
-//-(id)schoolPersonnel:(NSString *)username;
-//
-//-(id)newDesire:(NSString *)username;
  
 @end
